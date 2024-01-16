95ec3ec63a444f2b9c84fb8c984cf51cda653fa9
aslantasezgin <aslantasezginn@gmail.com>
2024-01-17 00:35:11 +0300

    dynamic datas added

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,61 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
-router.use("/blogs/:id",function(req,res){
-    res.render("users/blog-details")
-})
+const data = {
+  title: "Popüler Kurslar",
+  categories: [
+    "Web Geliştirme",
+    "Programlama",
+    "Mobil Uygulamalar",
+    "Veri Analizi",
+  ],
+  blogs: [
+    {
+      blog_id: 1,
+      blog_title: "Komple Uygulamalı Web Geliştirme",
+      blog_description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat cumque nihil, tempore adipisci nisi earum veniam eos unde. Dolor, incidunt?",
+      blog_img: "1.jpeg",
+      isHome: true,
+      isVisible:true,
+    },
+    {
+      blog_id: 2,
+      blog_title: "Pyhton Programlama",
+      blog_description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat cumque nihil, tempore adipisci nisi earum veniam eos unde. Dolor, incidunt?",
+      blog_img: "2.jpeg",
+      isHome: true,
+      isVisible:false,
 
-router.use("/blogs",function(req,res){
-    res.render("users/blogs")
-})
 
-router.use("/",function(req,res){
-    res.render("users/index")
-})
+    },
+    {
+      blog_id: 3,
+      blog_title: "Javascript Kursu",
+      blog_description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat cumque nihil, tempore adipisci nisi earum veniam eos unde. Dolor, incidunt?",
+      blog_img: "3.jpeg",
+      isHome: false,
+      isVisible:true,
 
-module.exports = router
+
+    },
+
+    
+  ],
+};
+
+router.use("/blogs/:id", function (req, res) {
+  res.render("users/blog-details");
+});
+
+router.use("/blogs", function (req, res) {
+  res.render("users/blogs", data);
+});
+
+router.use("/", function (req, res) {
+  res.render("users/index", data);
+});
+
+module.exports = router;
