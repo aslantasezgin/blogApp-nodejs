934108ef044024c9d8d47f207be4ff9fd61663c5
aslantasezgin <aslantasezginn@gmail.com>
2024-01-16 23:48:54 +0300

    template engine added

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,20 @@
 const express = require("express")
-const path = require("path")
 const router = express.Router()
 
-router.use("/blog/create",function(req,res){
-    res.sendFile(path.join(__dirname, "../views/admin", "blog-create.html"))
-})
 
-router.use("/blogs",function(req,res){
-    res.sendFile(path.join(__dirname, "../views/admin", "blog-list.html"))
+
+router.use("/blog/create",function(req,res){
+    res.render("admin/blog-create")
 })
 
 router.use("/blog/:blogid",function(req,res){
-    res.sendFile(path.join(__dirname, "../views/admin", "blog-edit.html"))
+    res.render("admin/blog-edit")
 })
+
+router.use("/blogs",function(req,res){
+    res.render("admin/blog-list")
+})
+
 
 
 module.exports = router
